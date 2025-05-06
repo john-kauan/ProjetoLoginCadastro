51df9b44a80688b920253c16f0c17c8732c2fb8c
johnkauan <johnkauanpassos123@gmail.com>
2025-05-06 16:47:29 -0300

    Criação incial da estrutura

---
+++ src/app/components/defaut-login-layout/defaut-login-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defaut-login-layout',
+  imports: [],
+  templateUrl: './defaut-login-layout.component.html',
+  styleUrl: './defaut-login-layout.component.scss'
+})
+export class DefautLoginLayoutComponent {
+
+}
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DefautLoginLayoutComponent } from '../../components/defaut-login-layout/defaut-login-layout.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [DefautLoginLayoutComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+  
+})
+export class LoginComponent {
+
+}
